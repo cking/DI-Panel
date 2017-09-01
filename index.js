67715fb74bc242f9dcfa1300565de1e7de25ce0b
Ch. König <criss@paars.de>
2017-09-01 08:04:19 +0200

    Added dotenv, bunyan, koa and marko

--- index.js
+++ index.js
@@ -1,90 +1,140 @@
-const express = require('express');
-const expressVue = require('express-vue');
-const http = require('http');
-const path = require('path');
-global.config = require('./config.json');
-const { Client, Pool } = require('pg');
-global.pg_pool = new Pool(config.db);
-global.helpers = require('./helpers');
-global.userMap = {};
-const WebsocketServer = require('./backend/websocket');
+// install marko's node require hook
+require('marko/node-require').install()
 
-const Strategy = require('passport-discord').Strategy;
-const passport = require('passport');
-const session = require('express-session');
+// load .env config file (extends process.env with custom env variables)
+require('dotenv-safe').load()
+if (!process.env.NODE_ENV) {
+  process.env.NODE_ENV = 'development'
+}
 
-const strategy = new Strategy({
+const path = require('path')
+
+// logger
+const { createLogger, stdSerializers } = require('bunyan')
+const logger = createLogger({
+  name: 'panel',
+  serializers: stdSerializers,
+  src: process.env.NODE_ENV === 'development',
+  stream: process.stderr,
+  level: process.env.NODE_ENV === 'development' ? 'debug' : 'warn'
+})
+
+// webapplication
+const Koa = require('koa')
+const mount = require('koa-mount')
+const app = new Koa()
+
+app.use(require('koa-static')(path.join(__dirname, 'public')))
+
+/*
+const express = require("express")
+const expressVue = require("express-vue")
+const path = require("path")
+global.config = require("./config.json")
+const { Client, Pool } = require("pg")
+global.pg_pool = new Pool(config.db)
+global.helpers = require("./helpers")
+global.userMap = {}
+const WebsocketServer = require("./backend/websocket")
+
+const Strategy = require("passport-discord").Strategy
+const passport = require("passport")
+const session = require("express-session")
+
+const strategy = new Strategy(
+  {
     clientID: global.config.appinfo.id,
     clientSecret: global.config.appinfo.secret,
     callbackURL: global.config.appinfo.callback,
-    scope: ['identify']
-}, (accessToken, refreshToken, profile, done) => {
+    scope: ["identify"],
+  },
+  (accessToken, refreshToken, profile, done) => {
     process.nextTick(() => {
-        return done(null, profile);
-    });
-});
+      return done(null, profile)
+    })
+  }
+)
 
-passport.use(strategy);
+passport.use(strategy)
 
-const app = express();
+const app = express()
 
-app.use(session({
+app.use(
+  session({
     secret: global.config.appinfo.othersecret,
     resave: false,
     saveUninitiated: true,
     cookie: {
-        secure: false,
-        maxAge: 86400000,
-        httpOnly: false
-    }
-}));
+      secure: false,
+      maxAge: 86400000,
+      httpOnly: false,
+    },
+  })
+)
 
-app.use(passport.initialize());
-app.use(passport.session());
+app.use(passport.initialize())
+app.use(passport.session())
 
 passport.serializeUser((user, done) => {
-    done(null, user);
-});
+  done(null, user)
+})
 
 passport.deserializeUser((obj, done) => {
-    done(null, obj);
-});
+  done(null, obj)
+})
 
-app.set('views', path.join(__dirname, 'app', 'views'));
-app.set('vue', {
-    componentsDir: path.join(__dirname, 'app', 'components'),
-    defaultLayout: 'layout'
-});
+app.set("views", path.join(__dirname, "app", "views"))
+app.set("vue", {
+  componentsDir: path.join(__dirname, "app", "components"),
+  defaultLayout: "layout",
+})
 
-app.engine('vue', expressVue);
-app.set('view engine', 'vue');
+app.engine("vue", expressVue)
+app.set("view engine", "vue")
 
-app.use(express.static('public'));
+app.use(express.static("public"))
+*/
 
-app.use('/', require('./routes/main'));
-app.use('/api', require('./routes/api'));
-
-app.get('/login', passport.authenticate('discord', {
-    scope: ['identify']
-}));
-app.get('/callback', passport.authenticate('discord', {
-    failureRedirect: '/'
-}), async (req, res) => {
-    console.log('A user has authenticated');
-    let rows = await global.pg_pool.query('SELECT token, salt FROM public.user WHERE id = $1', [req.user.id]);
-    if (rows.rows.length === 0) {
-
+// app.use('/api', require('./routes/api'))
+// TODO: app.use(mount('/api', null))
+// app.use("/", require("./routes/main"))
+const template = require('./client/index.marko')
+app.use(async (ctx, next) => {
+  ctx.type = 'html'
+  ctx.body = template.stream(
+    {
+      // TODO: pass required parameters!
     }
-    global.userMap[req.sessionID] = req.user.id;
-    res.redirect('/');
-});
+  )
+})
 
-const websocketServer = new WebsocketServer(app);
+/*
+TODO: jwt compat
+app.get(
+  "/login",
+  passport.authenticate("discord", {
+    scope: ["identify"],
+  })
+)
+app.get(
+  "/callback",
+  passport.authenticate("discord", {
+    failureRedirect: "/",
+  }),
+  async (req, res) => {
+    console.log("A user has authenticated")
+    let rows = await global.pg_pool.query("SELECT token, salt FROM public.user WHERE id = $1", [req.user.id])
+    if (rows.rows.length === 0) {
+    }
+    global.userMap[req.sessionID] = req.user.id
+    res.redirect("/")
+  }
+)
 
+const websocketServer = new WebsocketServer(app)
+*/
 
-const port = 8099;
-app.listen(port, () => {
-    console.log('App listening on port', port);
-});
+const port = process.env.PORT
+app.listen(port, () => logger.info({ port }, 'app listening'))
 
-require('./bot');
+// require('./bot')
